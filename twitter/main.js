ec7e213cc0f7658bbfe14fd50d47fe74436d0a40
Kevin Campos Venegas <kenderman.8@gmail.com>
2023-09-24 22:30:06 -0300

    un poco de todo

---
+++ twitter/main.js
@@ -0,0 +1,9 @@
+function laCosa() {
+    window.open("https://www.youtube.com/", "_blank");
+}
+
+document.getElementById("boton").addEventListener("click", laCosa);
+
+function laCosa() {
+    window.open("https://www.youtube.com/", "_blank");
+}
